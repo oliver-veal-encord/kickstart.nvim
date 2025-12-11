ddef752ce9d6b49bb6aaa04d6e0e38b49e7562fc
Oliver Veal <oliver.veal@encord.com>
2025-12-11 09:45:03 +0000

    remove unused

--- init.lua
+++ init.lua
@@ -268,31 +268,6 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
--- local function start_tsgo()
---   print 'Starting tsgo LSP...'
---   local root_files = { 'tsconfig.json', 'jsconfig.json', 'package.json', '.git' }
---   local paths = vim.fs.find(root_files, { stop = vim.env.HOME })
---   local root_dir = vim.fs.dirname(paths[1])
---
---   if root_dir == nil then
---     -- root directory was not found
---     return
---   end
---
---   vim.lsp.start {
---     name = 'tsgo',
---     cmd = { 'tsgo', '--lsp', '--stdio' },
---     root_dir = root_dir,
---     -- init_options = { hostInfo = "neovim" }, -- not implemented yet
---   }
--- end
---
--- vim.api.nvim_create_autocmd('FileType', {
---   pattern = { 'javascript', 'javascriptreact', 'javascript.jsx', 'typescript', 'typescriptreact', 'typescript.tsx' },
---   desc = 'Start tsgo LSP',
---   callback = start_tsgo,
--- })
-
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
